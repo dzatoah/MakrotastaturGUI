b4448b2e9a4e3fd6aefac846f0f41b2fed531b03
D0n1elT <daniel@teichm-sh.de>
2020-02-02 02:00:16 +0100

    build.sh: test for nuget as dependency

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+command -v npm   >/dev/null 2>&1 || { echo >&2 "I require npm & npx but it's not installed.  Aborting."; exit 1; }
+command -v npx   >/dev/null 2>&1 || { echo >&2 "I require npm & npx but it's not installed.  Aborting."; exit 1; }
+command -v nuget >/dev/null 2>&1 || { echo >&2 "I require nuget but it's not installed.  Aborting."; exit 1; }
+
 if [ -z $1 ]; then
 platform="";
 echo "Building for default platform"
